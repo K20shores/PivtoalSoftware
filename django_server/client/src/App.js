158391179581fafb8c95b89dfda3c886d742e618
Jonathon Hinchley <dev.jjh@planet-hinchley.com>
2018-10-01 12:48:27 -0500

    it works

--- django_server/client/src/App.js
+++ django_server/client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import SimpleMap from './SimpleMap';
 import SeverityList from './SeverityList';
-
+import L from 'leaflet'
 const patients = [
   {
     pulse: 120
@@ -16,15 +16,26 @@ const patients = [
     pulse: 0
   }
 ]
+
 class App extends Component {
   componentDidMount(){
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    //College station
+    //30.6280° N, 96.3344° W
+    const position = [30.6280, -96.334]
+    const map = L.map('mapid').setView(position, 13)
 
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map)
+    
+    L.marker(position)
+      .addTo(map)
+      .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
   }
   render() {
     return (
       <div style={{display: 'flex', flexDirection: 'row'}}>
-         <div style={ { height: '180px' }} id="mapid"></div>
+        <div style={ { width: '720px', height: '960px' }} id="mapid"></div>
         <SeverityList
           patients={patients}
         />
