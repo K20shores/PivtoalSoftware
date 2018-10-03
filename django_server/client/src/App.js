9286fda2a519886405857b180043e72eb5602ecd
Jonathon Hinchley <dev.jjh@planet-hinchley.com>
2018-10-03 13:11:30 -0500

    static patient icons display

--- django_server/client/src/App.js
+++ django_server/client/src/App.js
@@ -2,35 +2,48 @@ import React, { Component } from 'react';
 import SimpleMap from './SimpleMap';
 import SeverityList from './SeverityList';
 import L from 'leaflet'
+import {icons, blackIcon} from './Marker';
 const patients = [
   {
+    location: [30.6280, -96.334],
+    severity: 1,
     pulse: 120
   },
   {
+    location: [30.5910, -96.3640],
+    severity: 0,
     pulse: 40
   },
   {
+    location: [30.6280, -96.350],
+    severity: 2,
     pulse: 180
   },
   {
+    location: [30.659, -96.334],
+    severity: -1,
     pulse: 0
   }
 ]
 
 class App extends Component {
   componentDidMount(){
-    //College station
-    //30.6280° N, 96.3344° W
+
+    //College station 30.6280° N, 96.3344° W
     const position = [30.6280, -96.334]
     const map = L.map('mapid').setView(position, 13)
 
+    //TODO: get this from backend instead
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map)
     
-    L.marker(position)
-      .addTo(map)
-      .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
+    patients.forEach( patient => {
+      L.marker(patient.location, {icon: patient.severity != -1 ? icons[patient.severity] : blackIcon})
+        .addTo(map)
+        .bindPopup(`${patient.pulse}<br> Easily customizable.`)
+    })
+    
   }
   render() {
     return (
--- django_server/client/src/Marker.js
+++ django_server/client/src/Marker.js
@@ -1,4 +1,55 @@
 
 import React from 'react'
+import L from 'leaflet'
 const Marker = ({ text,severity }) => <div style={{color: severity <= 1 ? 'yellow' : 'red' }}>{text}</div>;
 export default Marker;
+
+const greenIcon = L.icon({
+    iconUrl: 'green-marker-icon.png',
+    // shadowUrl: 'leaf-shadow.png',
+
+    iconSize:     [28, 41], // size of the icon
+    // shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+const yellowIcon = L.icon({
+    iconUrl: 'yellow-marker-icon.png',
+    // shadowUrl: 'leaf-shadow.png',
+
+    iconSize:     [28, 41], // size of the icon
+    // shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+const redIcon = L.icon({
+    iconUrl: 'red-marker-icon.png',
+    // shadowUrl: 'leaf-shadow.png',
+
+    iconSize:     [28, 41], // size of the icon
+    // shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+export const blackIcon = L.icon({
+    iconUrl: 'black-marker-icon.png',
+    // shadowUrl: 'leaf-shadow.png',
+
+    iconSize:     [28, 41], // size of the icon
+    // shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+export const icons = [
+    greenIcon,
+    yellowIcon,
+    redIcon
+]
