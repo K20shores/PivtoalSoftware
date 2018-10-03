0acf98d8e56d3ff1bc3bafa23eb39052c28809e1
Jonathon Hinchley <dev.jjh@planet-hinchley.com>
2018-10-03 13:28:21 -0500

    changed to function so it is easier to call

--- django_server/django_server/write_to_data_server.py
+++ django_server/django_server/write_to_data_server.py
@@ -1,11 +1,18 @@
 import pymongo
 
-client = pymongo.MongoClient('mongodb://localhost:27017')
-db = client['PIVOTAL_DB']
-col = db["NodeData"]
-
-entry = {"nodeID": 1, "x_coord": 23.7, "y_coord": 33.7,
- "z_coord": 43.7, "biometric_data": 12, "time": 1600, 
- "battery": 99, "priority": 1}
+def writeData(nodeID, x_coord, y_coord, z_coord, pulse, time, priority):
+    client = pymongo.MongoClient('mongodb://localhost:27017')
+    db = client['PIVOTAL_DB']
+    col = db["NodeData"]
+    entry = {
+        "nodeID": nodeID, 
+        "x_coord": x_coord, 
+        "y_coord": y_coord,
+        "z_coord": z_coord, 
+        "biometric_data": pulse, 
+        "time": time, 
+        "battery": 99, 
+        "priority": priority
+    }
  
-x = col.insert_one(entry)
+    x = col.insert_one(entry)
