4dbe716b7c9133f8237efeaec0bcda9582dd0fd0
Jonathon Hinchley <dev.jjh@planet-hinchley.com>
2018-10-22 13:13:53 -0500

    change to accept dict

--- django_server/DataManager.py
+++ django_server/DataManager.py
@@ -5,21 +5,12 @@ DB_URL = 'mongodb://localhost:27017'
 DB_NAME = 'PIVOTAL_DB'
 DB_COLLECTION = 'NodeData'
 
-def writeData(nodeID, x_coord, y_coord, z_coord, pulse, time, priority):
+def writeData(entry):
     try:
         client = pymongo.MongoClient(DB_URL)
         db = client[DB_NAME]
         col = db[DB_COLLECTION]
-        entry = {
-            "nodeID": nodeID, 
-            "x_coord": x_coord, 
-            "y_coord": y_coord,
-            "z_coord": z_coord, 
-            "biometric_data": pulse, 
-            "time": time, 
-            "battery": 99, 
-            "priority": priority
-        }
+        
         x = col.insert_one(entry)
     except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to server")
