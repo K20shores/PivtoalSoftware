0f7d6249e9ef42d36cb011a1e97d8be67ce61c64
Kyle Shores <kyle.shores44@gmail.com>
2018-11-14 13:32:35 -0600

    fixing duplicate map markers

--- client/src/redux/resources.js
+++ client/src/redux/resources.js
@@ -9,28 +9,11 @@ export default function reducer(state = INITIAL_STATE, action = {}){
         case SET_MARKERS:
         {
             //remove old layers
-            for (let index = 0; index < state.markers; index++) {
-                action.map.removeLayer(state.markers[index])
-
-            }
-
+            Object.keys(state.markers).map(function(key, index) {
+                action.map.removeLayer(state.markers[key])
+            });
             let newMarkers = {}
-            // let resources = [
-            //   {
-            //     "nodeID": 1,
-            //     "x_coord": 30.628,
-            //     "y_coord": -96.334,
-            //     "resource_type": 'water',
-            //     "resource_amount": 5
-            //   },
-            //   {
-            //     "nodeID": 2,
-            //     "x_coord": 30.629,
-            //     "y_coord": -96.334,
-            //     "resource_type": 'bread',
-            //     "resource_amount": 5
-            //   }
-            // ]
+
             action.resources.forEach( resource => {
                 resource['location'] = {
                     'lat': resource.x_coord,
@@ -38,12 +21,12 @@ export default function reducer(state = INITIAL_STATE, action = {}){
                 }
                 let m = L.marker(resource.location, {icon:  blackIcon})
                 //let m = L.marker(resource.location, {icon: resource.name !== '' ? icons[resource.name] : blackIcon})
-                console.log(resource);
-                //console.log(m,action.map);
                 m.addTo(action.map)
                 m.bindPopup(`Type: ${resource.resource_type}<br>
                   Amount:  ${resource.resource_amount} <br>
-                  Lat/Long: ${resource.x_coord}/${resource.y_coord}`)
+                  Lat/Long: ${resource.x_coord}/${resource.y_coord}
+                  Last Updated: ${resource.time}
+                    `)
                 newMarkers[resource.nodeID] = m;
             })
             return {...state, markers: newMarkers, resources: action.resources}
